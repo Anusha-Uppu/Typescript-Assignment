951c5141fcd20ec1ee5eb404b30905e11dd1a235
Anusha Uppu <admin@Anusha-Uppu.local>
2024-05-20 16:36:03 +0530

    after excuteting add ing index.js file

---
+++ index.js
@@ -0,0 +1,22 @@
+var address1 = {
+    city: "Bhadrachalam",
+    state: "Telangana",
+    pincode: 507111
+};
+var student1 = {
+    name: "Anusha",
+    age: 21,
+    email: "anusha.uppu@everest.engineering",
+    courselist: "typescript",
+    address: address1
+};
+var student2 = {
+    name: "keerthana",
+    age: 21,
+    email: "keerthana.durgam@everest.engineering",
+    courselist: "javascript",
+    address: address1
+};
+console.log(student1);
+console.log(student2);
+// using type 
